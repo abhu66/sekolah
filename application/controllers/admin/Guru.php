1e1a42f70acec2dce0068e7afee202a5a51cdddf
abhu66 <khoirulabhu@gmail.com>
2017-01-10 01:07:42 +0700

    bang w cuma bikin Controller guru. coba cek w g tau lagi,masih butuh bimbingan

---
+++ application/controllers/admin/Guru.php
@@ -0,0 +1,24 @@
+<?php
+if(defined('BASEPATH')) exit ('Anda tidak dijinkan ke situs ini');
+
+class Guru extends CI_Controller{
+	
+	public function __construct()
+	{
+		parent:: __construct();
+		$this->load->model('Guru_model');
+		$this->load->helper('url');
+		if($this->session->userdata('logged') == NULL){
+			header('Location:'.site_url('admin/auth/login') . '?location:' .urlencode($_SERVER['REQUEST_URI']));
+		}
+	}
+	public function index($offset = NULL)
+		{
+		$this->load->library('pagination');
+		$data['title'] = 'Guru List';
+		$data['guru'] = $this->Guru_model->get(array('status'=>TRUE));
+		$data['main'] = 'admin/User/user_view';
+		$this->load->view('admin/layout',$data); 
+	    
+	}
+}
